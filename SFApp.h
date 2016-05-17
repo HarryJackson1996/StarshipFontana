65852dd41940298cead8daa15d613312fac3775f
HarryJackson1996 <harryjackson001@hotmail.co.uk>
2016-05-17 13:31:14 +0100

    Add files via upload
    Initial Commit

---
+++ SFApp.h
@@ -0,0 +1,54 @@
+#ifndef SFAPP_H
+#define SFAPP_H
+
+#include <memory>   // Pull in std::shared_ptr
+#include <iostream> // Pull in std::cerr, std::endl
+#include <list>     // Pull in list
+#include <sstream>
+
+
+using namespace std;
+
+#include "SFCommon.h"
+#include "SFEvent.h"
+#include "SFAsset.h"
+
+/**
+ * Represents the StarshipFontana application.  It has responsibilities for
+ * * Creating and destroying the app window
+ * * Processing game events
+ */
+class SFApp {
+public:
+  SFApp(std::shared_ptr<SFWindow>);
+  virtual ~SFApp();
+  void    OnEvent(SFEvent &);
+  int     OnExecute();
+  void    OnUpdateWorld();
+  void    OnRender();
+
+  void    FireProjectile();
+
+private:
+  bool                    is_running;
+
+  shared_ptr<SFWindow>       sf_window;
+  shared_ptr<SFAsset>        player;
+  shared_ptr<SFBoundingBox>  app_box;
+  list<shared_ptr<SFAsset> > projectiles;
+  list<shared_ptr<SFAsset> > projectiles2;
+  list<shared_ptr<SFAsset> > aliens;
+  list<shared_ptr<SFAsset> > walls;
+  list<shared_ptr<SFAsset> > walls2;
+  list<shared_ptr<SFAsset> > bigCoins; 
+  list<shared_ptr<SFAsset> > smallCoins;
+  list<shared_ptr<SFAsset> > bombs;
+  list<shared_ptr<SFAsset> > alienBosses;
+  int coinCounter;
+  int fire;
+  int score;
+  int counter;
+  int alienCounter;
+  int health1;
+};
+#endif
---
+++ SFEvent.cpp
@@ -0,0 +1,46 @@
+#include "SFEvent.h"
+
+
+/**
+ * Effectively wraps an SDL_Event in our custom event type.
+ */
+SFEvent::SFEvent(const SDL_Event & event) {
+  switch (event.type) {
+  case SDL_QUIT:
+    code = SFEVENT_QUIT;
+    break;
+  case SDL_USEREVENT:
+    code = SFEVENT_UPDATE;
+    break;
+  case SDL_KEYDOWN:
+    switch (event.key.keysym.sym) {
+    case SDLK_a:
+      code = SFEVENT_PLAYER_LEFT;
+      break;
+    case SDLK_d:
+      code = SFEVENT_PLAYER_RIGHT;
+      break;
+    case SDLK_w:
+      code = SFEVENT_PLAYER_UP;
+      break;
+    case SDLK_s:
+      code = SFEVENT_PLAYER_DOWN;
+      break;
+    case SDLK_SPACE:
+      code = SFEVENT_FIRE;
+      break;
+    case SDLK_q:
+      code = SFEVENT_QUIT;
+      break;
+    }
+    break;
+  default:
+    code = SFEVENT_NULL;
+    break;
+  }
+}
+
+
+SFEVENT SFEvent::GetCode() {
+  return code;
+}
